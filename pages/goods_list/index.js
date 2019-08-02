7cf34f82b805ed98df9bfdbe35708f068aa39c6c
cyt <470922684@qq.com>
2019-08-02 14:55:35 +0800

    商品列表：上拉触底、下拉刷新

--- pages/goods_list/index.js
+++ pages/goods_list/index.js
@@ -1,9 +1,7 @@
 // pages/goods_list/index.js
 import { request } from "../../request/index.js";
 Page({
-  /**
-   * 页面的初始数据
-   */
+  // 页面的初始数据
   data: {
     // 传给tabs子组件的数据
     tabs: [
@@ -22,21 +20,32 @@ Page({
     pagenum: 1, //页码
     pagesize: 10 //页容量
   },
-  // 总页数
-  TotalPages:1,
+  TotalPages:1, // 总页数
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
+  // 生命周期函数--监听页面加载
   onLoad: function (options) {
     console.log(options,'onLoad');
     this.QueryParams.cid = options.cid; // 接收url传递过来的参数,给全局参数赋值
     this.getGoodsList(); //发送获取商品列表数据的请求
   },
 
-  /**
-   * 子组件触发的自定义事件
-   */
+  // 页面事件处理函数--监听用户上拉触底事件
+  onReachBottom() {
+    // console.log("监听用户上拉触底事件");
+    // 1 判断有没有下一页数据
+    if (this.QueryParams.pagenum >= this.TotalPages) {
+      wx.showToast({
+        title: '没有下一页了哦',
+        icon: 'none',
+      });
+    } else {
+      console.log("还有下一页数据");
+      this.QueryParams.pagenum++;
+      this.getGoodsList();
+    }
+  },
+
+  // 子组件触发的自定义事件
   handleItemChange(e) {
     const { index } = e.detail;
     let { tabs } = this.data;
@@ -57,7 +66,17 @@ Page({
           // 拼接数组
           goodsList:[...this.data.goodsList,...result.goods]
         })
-        
+        // 获取到数据后，结束下拉刷新
+        wx.stopPullDownRefresh();
       })
+  },
+
+  // 下拉刷新事件
+  onPullDownRefresh() {
+    this.QueryParams.pagenum = 1; // 重置页码
+    this.setData({
+      goodsList:[] //重置数组
+    })
+    this.getGoodsList(); // 从新发送请求，获取最新数据
   }
 });
