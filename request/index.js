45fe5f8ea1a90b0bb3bda3b0cdf50deae05b0e3f
cyt <470922684@qq.com>
2019-08-02 15:40:01 +0800

    商品列表：全局等待缓存图标

--- request/index.js
+++ request/index.js
@@ -1,5 +1,14 @@
+// 同时发送请求的ajax的次数
+let ajaxTimes = 0;
+
 // 封装公共请求接口
 export const request = params => {
+  ajaxTimes++;
+  // 显示正在等待图标
+  wx.showLoading({
+    title: "加载中"
+  });
+
   const baseUrl = "https://api.zbztb.cn/api/public/v1";
   return new Promise((resolve, reject) => {
     wx.request({
@@ -11,6 +20,14 @@ export const request = params => {
       },
       fail: err => {
         reject(err);
+      },
+      complete: () => {
+        ajaxTimes--;
+        if(ajaxTimes===0)
+        // 关闭正在等待图标
+          wx.hideLoading();
+        console.log(ajaxTimes);
+        
       }
     });
   });
