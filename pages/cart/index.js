06647a8ef61ed91fff8f61b1f984f63d56048144
cyt <470922684@qq.com>
2019-08-05 11:11:55 +0800

    购物车：动态渲染

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -1,12 +1,22 @@
 import regeneratorRuntime from "../../lib/runtime/runtime";
 import { openSetting, getSetting, chooseAddress } from "../../utils/async";
+import { getStorageCart,setStorageCart,getStorageAddress,setStorageAddress } from "../../utils/storage";
+
 
 Page({
   data: {
-    address: {}
+    address: {}, //添加的数据
+    cart: {} //购物车数据
   },
 
   onLoad: function(options) {},
+
+  onShow() {
+    const address = getStorageAddress() || {};
+    const cart = getStorageCart() || {};
+    this.setData({ address, cart });
+    // this.setData(cart);
+  },
 
   // 添加添加收货地址
   async handleChooseAddress() {
@@ -19,9 +29,6 @@ Page({
     } else {
       // 还没授权 =》 打开授权页面
       await openSetting();
-      // 直接调用收货地址
-      // const result2 = await chooseAddress();
-      // console.log(result2);
     }
     const result2 = await chooseAddress();
     // 把新增的地址信息存入本地存储中
@@ -30,9 +37,6 @@ Page({
       result2.cityName +
       result2.countyName +
       result2.detailInfo;
-    wx.setStorageSync("address", result2);
-  },
-  onShow() {
-    this.setData({address:wx.getStorageSync("address")})
+      setStorageAddress(result2)
   }
 });
--- utils/storage.js
+++ utils/storage.js
@@ -4,17 +4,27 @@
 export const getStorageCate = () => {
   return wx.getStorageSync("cate")
 }
-
 // 设置分类商品数据到本地存储中
 export const setStorageCate = (obj) => {
   wx.setStorageSync("cate", obj);
 }
+
 
 // 获取本地存储中购物车的数据
 export const getStorageCart = () => {
   return wx.getStorageSync("cart"); 
 }
 // 设置购物车到本地存储中   obj 要填充的数据
-export const setStorageCart = (obj) => {
-  wx.setStorageSync("cart", obj);
+export const setStorageCart = (cart) => {
+  wx.setStorageSync("cart", cart);
+}
+
+
+// 获取本地存储中地址的数据
+export const getStorageAddress = () => {
+  return wx.getStorageSync("address"); 
+}
+// 设置地址到本地存储中   address 要填充的数据
+export const setStorageAddress = (address) => {
+  wx.setStorageSync("address", address);
 }
